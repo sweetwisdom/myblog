8bd56efc4697dafe954f9428159dba7333d216aa
sweetwisdom <sweetwisdom@qq.com>
2020-06-02 18:52:53 +0800

    bug2

--- deploy.sh
+++ deploy.sh
@@ -1,35 +1,23 @@
-#!/usr/bin/env sh
+# #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
+# # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
+# # 生成静态文件
 npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# deploy to github
-# echo 'sweet.cartoom.club' > CNAME
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com/sweetwisdom/myblog.git
-else
-  msg='来自github actions的自动部署'
-  githubUrl=https://sweetwisdom:${GITHUB_TOKEN}@github.com/sweetwisdom/myblog.git
-  git config --global user.name "sweetwisdom"
-  git config --global user.email "sweetwisdom@qq.com"
-fi
-git init
-git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
+
 
 # deploy to coding
 # echo 'sweet.cartoom.club' > CNAME  # 自定义域名
 if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+ msg='部署'
   codingUrl=git@e.coding.net:sweetwisdom/coding-demo.git
 else
+ msg='来自github actions的自动部署 20200602'
   codingUrl=https://ZULMWvoZDe:${CODING_TOKEN}@e.coding.net/sweetwisdom/coding-demo.git
 fi
 git add -A
