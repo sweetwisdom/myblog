444c74c0dae19b3c45d288f84097764645ab9b77
sweetwisdom <sweetwisdom@qq.com>
2020-06-02 19:35:45 +0800

    bug.3

--- deploy copy.sh
+++ deploy copy.sh
@@ -1,21 +1,7 @@
-#!/usr/bin/env sh
+ set -e
+msg='部署'
+ echo "Hello, Shell"
+  echo $msg
+sleep 3s
+date +%F" "%H:%M:%S
 
-# 确保脚本抛出遇到的错误
-set -e
-
-cd zc
-
-
-git init
-git config --global user.name "sweetwisdom"
-git config --global user.email "sweetwisdom@qq.com"
-git add -A
-git commit -m "test"
-# git push -f $githubUrl master:gh-pages # 推送到github
-
-# deploy to coding
-# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-
-git push -f git@nodeing666:sweetwisdom/myblog.git master # 推送到coding
-
-cd - # 退回开始所在目录
--- deploy.sh
+++ deploy.sh
@@ -14,17 +14,24 @@ cd docs/.vuepress/dist
 # deploy to coding
 # echo 'sweet.cartoom.club' > CNAME  # 自定义域名
 if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+
  msg='部署'
+   echo $msg
   codingUrl=git@e.coding.net:sweetwisdom/coding-demo.git
 else
  msg='来自github actions的自动部署 20200602'
   codingUrl=https://ZULMWvoZDe:${CODING_TOKEN}@e.coding.net/sweetwisdom/coding-demo.git
+
+    echo $msg
+    sleep 2s
+    echo $CODING_TOKEN
 fi
 git config --global user.name "sweetwisdom"
 git config --global user.email "sweetwisdom@qq.com"
-
+  echo 'git'
 git add -A
 git commit -m "${msg}"
+  echo 'git-2'
 git push -f $codingUrl master # 推送到coding
 
 cd - # 退回开始所在目录
