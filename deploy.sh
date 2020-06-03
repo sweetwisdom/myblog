10ff1e24895600716aa848e67f35ac6ed581a780
sweetwisdom <sweetwisdom@qq.com>
2020-06-03 09:08:00 +0800

    自动部署

--- deploy.sh
+++ deploy.sh
@@ -16,11 +16,13 @@ cd docs/.vuepress/dist
 # echo 'sweet.cartoom.club' > CNAME  # 自定义域名
 if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
 
- msg='部署'
+ msg='手动部署于'$(date "+%Y-%m-%d %H:%M:%S")
    echo $msg
   codingUrl=git@e.coding.net:sweetwisdom/coding-demo.git
 else
- msg='来自github actions的自动部署 20200602'
+
+
+ msg='来自github actions的自动部署于'$(date "+%Y-%m-%d %H:%M:%S")
   codingUrl=https://ZULMWvoZDe:${CODING_TOKEN}@e.coding.net/sweetwisdom/coding-demo.git
 
     echo "正在最后部署"
@@ -32,8 +34,8 @@ git config --global user.name "sweetwisdom"
 git config --global user.email "sweetwisdom@qq.com"
   echo 'git'
 git add -A
-git commit -m "自动部署"
-  echo '正在自动部署0602'
+git commit -m "${msg}"
+  echo '正在自动部署'$(date "+%Y-%m-%d %H:%M:%S")
 git push -f $codingUrl master # 推送到coding
 
 cd - # 退回开始所在目录
