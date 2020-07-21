9f0e303f4607272b6e63ed30fe0557b988b4960b
sweetwisdom <sweetwisdom@qq.com>
2020-07-21 15:45:31 +0800

    手动更新码云于2020-07-21 15:45:30

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 const nav = require("./config/nav.js");
 
 module.exports = {
-  // dest: "./myblog/", //输出文件夹名称
-  // base: "/myblog/", //myblog
+  dest: "./myblog/", //输出文件夹名称
+  base: "/myblog/", //myblog
   //  提交到github需要屏蔽此更新
 
   port: "1234",
